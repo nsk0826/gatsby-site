30f5bcc4e155a784480f3f8cd504cf4d596a752b
nsk0826 <nsk.20160401@gmail.com>
2022-01-09 01:04:12 +0900

    add footer

---
+++ src/components/footer.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const Footer = () => {
+  return (
+    <footer>
+      <div>© Gatsby Blog Site { (new Date()).getFullYear() }</div>
+    </footer>
+  )
+}
+
+export default Footer;
