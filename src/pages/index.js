c597f5e1eab91b6958d90b47e872a8b992865e2c
nsk0826 <nsk.20160401@gmail.com>
2022-01-09 00:59:37 +0900

    add Link

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,8 +1,12 @@
-import React from "react"
+import React from "react";
+import { Link } from "gatsby";
 
 const BlogPage = () => {
   return (
     <div>
+      <ul>
+        <Link to="/about">About</Link>
+      </ul>
       <h1>Gatsby Blog Site</h1>
     </div>
   )
