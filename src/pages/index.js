b9da34f216e32289a9210b6a3fa41909d2f78dbb
nsk0826 <nsk.20160401@gmail.com>
2022-01-09 01:05:06 +0900

    add Footer

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,11 +1,13 @@
 import React from "react";
 import Header from "../components/header.js";
+import Footer from "../components/footer.js";
 
 const BlogPage = () => {
   return (
     <div>
       <Header/>
       <h1>Gatsby Blog Site</h1>
+      <Footer/>
     </div>
   )
 }
