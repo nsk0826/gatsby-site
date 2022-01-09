bf4d7072d89194dd9ef6dafa8efbbafda44a3175
nsk0826 <nsk.20160401@gmail.com>
2022-01-10 00:02:33 +0900

    mod config

--- gatsby-config.js
+++ gatsby-config.js
@@ -5,6 +5,12 @@
  */
 
 module.exports = {
+  siteMetadata: {
+    title: "はじめてのGatsby Site",
+    author: "Reffect",
+    category: ["Laravel", "Vue.js", "React"],
+    user: { name: "John Doe", email: "john@example.com" },
+  },
   /* Your site config here */
   plugins: [],
 }
