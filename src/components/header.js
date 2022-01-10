68082efad2b2dd527fe742d4e84225b48f2d91b6
nsk0826 <nsk.20160401@gmail.com>
2022-01-10 22:59:32 +0900

    add graphql in header

--- src/components/header.js
+++ src/components/header.js
@@ -1,9 +1,23 @@
 import React from 'react';
-import { Link } from 'gatsby';
+import { useStaticQuery, Link } from 'gatsby';
+import { graphql } from 'gatsby';
 
 const Header = () => {
+  const data = useStaticQuery(
+    graphql`
+      query {
+        site {
+          siteMetadata {
+            title
+          }
+        }
+      }
+    `
+  )
+
   return (
     <header>
+      <h2>{data.site.siteMetadata.title}</h2>
       <nav>
         <ul>
           <li><Link to="/">top</Link></li>
