02c365d9b099e4bc450f97ef29360a54f9022c39
nsk0826 <nsk.20160401@gmail.com>
2022-01-10 23:49:45 +0900

    mod about page

--- src/pages/about.js
+++ src/pages/about.js
@@ -2,24 +2,47 @@ import React from "react"
 import Layout from "../components/layout"
 import { graphql } from 'gatsby'
 
-const AboutPage = ( {data} ) => {
+const About = ( {data} ) => {
+  console.log(data)
   return (
     <div>
       <Layout>
-        <h2>{data.site.siteMetadata.title}</h2>
-        <h1>About Page!!</h1>
+        <h1>About Page</h1>
+        <table>
+          <thead>
+            <tr>
+              <th>パス</th>
+              <th>サイズ</th>
+              <th>作成時間</th>
+            </tr>
+          </thead>
+          <tbody>
+            {data.allFile.nodes.map(node => (
+              <tr>
+                <td>{node.relativePath}</td>
+                <td>{node.size}</td>
+                <td>{node.ctime}</td>
+              </tr>
+            ))}
+          </tbody>
+        </table>
       </Layout>
     </div>
   )
 }
 
 export const query = graphql`
-  query{
-    site{
-      siteMetadata{
-        title
+  {
+    allFile {
+      totalCount
+      nodes {
+        relativePath
+        size
+        name
+        extension
+        ctime
       }
     }
   }
 `
-export default AboutPage;
+export default About;
