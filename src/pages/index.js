13e7d229eeac8f13e6d5b908ac8f3b1a0329412b
nsk0826 <nsk.20160401@gmail.com>
2022-01-11 00:18:39 +0900

    マークダウンファイルの追加

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,14 +1,35 @@
 import React from "react";
 import Layout from "../components/layout.js";
+import { graphql } from "gatsby";
 
-const BlogPage = () => {
+export default function Home({ data }) {
   return (
     <div>
       <Layout>
-        <h1 className="title">Gatsby Blog Site</h1>
+        <h1>Gatsby Blog Site</h1>
+        {data.allMarkdownRemark.nodes.map(node => (
+          <div key={node.id}>
+            <h2>{node.frontmatter.title}</h2>
+            <p>{node.frontmatter.dare}</p>
+            <div dangerouslySetInnerHTML={{ __html: node.html }} />
+          </div>
+        ))}
       </Layout>
     </div>
   )
 }
-
-export default BlogPage;
+export const query = graphql`
+  {
+    allMarkdownRemark {
+      nodes {
+        id
+        html
+        timeToRead
+        frontmatter {
+          date
+          title
+        }
+      }
+    }
+  }
+`
