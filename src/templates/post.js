9affa0e0cdf1310cb8ee75768d91904b3d62d9b8
nsk0826 <nsk.20160401@gmail.com>
2022-01-15 02:22:29 +0900

    add template.js

---
+++ src/templates/post.js
@@ -0,0 +1,10 @@
+import React from "react"
+import Layout from "../components/layout"
+
+export default function Post(){
+    return (
+        <Layout>
+            <div>ブログ記事のコンテンツ</div>
+        </Layout>
+    )
+}
