8ccf8bf44aa51622cebd5cd7da31176d3115ba4c
nsk0826 <nsk.20160401@gmail.com>
2022-01-17 23:52:37 +0900

    add graphql

--- src/templates/post.js
+++ src/templates/post.js
@@ -1,10 +1,23 @@
 import React from "react"
+import { graphql } from "gatsby"
 import Layout from "../components/layout"
 
-export default function Post(){
-    return (
-        <Layout>
-            <div>ブログ記事のコンテンツ</div>
-        </Layout>
-    )
+export default function Post({ data }){
+  return (
+    <Layout>
+      <h1>{data.markdownRemark.frontmatter.title}</h1>
+      <div dangerouslySetInnerHTML={{ __html:
+      data.markdownRemark.html }} />
+    </Layout>
+  )
 }
+export const query = graphql`
+  query($slug: String!) {
+    markdownRemark(fields: { slug: { eq: $slug } }) {
+      html
+      frontmatter {
+        title
+      }
+    }
+  }
+`
