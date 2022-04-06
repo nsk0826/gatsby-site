d6cff98b7ed04377d6f274e1dc28e1b5bdeb6de7
nsk0826 <nsk.20160401@gmail.com>
2022-04-07 00:47:15 +0900

    cms を表示

--- src/components/layout.js
+++ src/components/layout.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import Header from './header';
 import Footer from './footer';
+import cmsTestPage from '../pages/cmsTest';
 import "./layout.css";
 
 const Layout = ( props ) => {
@@ -9,6 +10,7 @@ const Layout = ( props ) => {
       <Header/>
         { props.children }
       <Footer/>
+      <cmsTestPage/>
     </div>
   )
 }
